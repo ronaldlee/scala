ebab49fec01a91b3fe7ef0bb636b731c40886d55
ronaldlee <ronaldylee@gmail.com>
2021-03-22 09:59:33 -0700

    append prepend list

--- data_structures/src/main/scala/ListTest.scala
+++ data_structures/src/main/scala/ListTest.scala
@@ -1,10 +1,25 @@
+import scala.::
+
 object ListTest {
-  val l = Vector("A", "B", "C", "D", "E")
+//  val l = Vector("A", "B", "C", "D", "E")
+//
+//  println("head: "  + l.head)
+//  println("tail: " + l.tail)
+//
+//  val l2 = l.sliding(2,3).toList
+//  println("slide: " + l2)
 
-  println("head: "  + l.head)
-  println("tail: " + l.tail)
+  //prepend list
+  var l = List("A", "B")
+  l = "1" :: l
+  println("prepend: " + l)
 
-  val l2 = l.sliding(2,3).toList
-  println("slide: " + l2)
+  //append
+  l = l :+ "C"
+  println("append: " + l)
+
+  //prepend with +:
+  l = "0" +: l
+  println("prepend: " + l)
 
 }
